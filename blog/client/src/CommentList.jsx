0e330c8f904f193a41d84cf2068446c849c752fb
raphhawk <bing619.at@gmail.com>
2023-05-30 11:55:28 +0530

    moderation init

--- blog/client/src/CommentList.jsx
+++ blog/client/src/CommentList.jsx
@@ -1,8 +1,12 @@
 export const CommentList = ({ comments }) => {
   const renderedComments = Object.values(comments).map((comment) => {
     return (
-      <li key={comment.id} className="ml-5">
-        {". " + comment.content}
+      <li key={comment.id} className="ml-5 list-disc">
+        {comment.status === "approved" ? (
+          comment.content
+        ) : (
+          <span className="italic">{"comment " + comment.status}</span>
+        )}
       </li>
     );
   });
